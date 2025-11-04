4db32fb9dac378fc2e6d6bd8845badd1f5790200
mvdevelop <marcosvmdilly@gmail.com>
2025-11-04 10:22:52 -0300

    working on contact.jsx

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -9,17 +9,17 @@ import { BiPhone, BiEnvelope, BiMap, BiTime, BiMessageDetail, BiGlobe } from 're
 export default function Contact() {
   return (
     <>
-        <section className='px-[8%] lg:px-[16%] py-8 text-white'>
+        <section className='px-[8%] lg:px-[16%] py-5 text-white'>
             <div className='grid lg:grid-cols-2 gap-12 w-full'>
 
                 {/* Contact Form */}
-                <div className='bg-gray-500/5 p-7 rounded-xl shadow-lg'>
-                    <h2 className='text-3xl font-unbounded font-normal text-[color:var(--primary-color)] mb-2'>Let`s Work Together</h2>
-                    <p className='text-sm text-gray-400 my-7'>
+                <div className='bg-gray-500/5 p-5 rounded-xl shadow-lg'>
+                    <h2 className='text-3xl font-unbounded font-normal text-[color:var(--primary-color)] mb-3'>Let`s Work Together</h2>
+                    {/* <p className='text-sm text-gray-400 my-7'>
                         Collaboration is where the best ideas come to life!
-                    </p>
+                    </p> */}
 
-                    <form className='space-y-5'>
+                    <form className='space-y-7'>
                         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                             <input type="text" placeholder='First Name' className='w-full bg-[#0e0f12] border border-gray-700 rounded-lg px-4 py-4 text-sm focus:outline-none focus:border-[var(--primary-color)] transition-all duration-500' />
                             <input type="text" placeholder='Last Name' className='w-full bg-[#0e0f12] border border-gray-700 rounded-lg px-4 py-4 text-sm focus:outline-none focus:border-[var(--primary-color)] transition-all duration-500' />
@@ -37,8 +37,16 @@ export default function Contact() {
 
                         <textarea rows={3} placeholder='Send a Message' className='w-full bg-[#0e0f12] border border-gray-700 rounded-lg px-4 py-4 text-sm focus:outline-none focus:border-[var(--primary-color)] transition-all duration-500'></textarea>
 
-                        <button type='submit' className='bg-[var(--primary-color)] hover:bg-white text-gray-700 px-6 py-3 mt-5 rounded-lg font-semibold transition-all duration-500 cursor-pointer'>Send Message</button>
+                        <button type='submit' className='bg-[var(--primary-color)] hover:bg-white text-white hover:text-black px-6 py-3 mt-0 rounded-lg font-semibold transition-all duration-500 cursor-pointer'>Send Message</button>
                     </form>
+                </div>
+
+                {/* Contact Info */}
+                <div className='flex flex-col space-y-6 justify-center'>
+                    
+                    <div className='flex items-center space-x-4 border-b border-gray-600 pb-5'>
+                        <span className='bg-[#1a1b1f] p-4 rounded-lg text-[color:var(--primary-color)]'><BiPhone/></span>
+                    </div>
                 </div>
             </div>
         </section>
